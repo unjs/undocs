47de41490daec9a32bf85aa9963c74a66ead8333
Sébastien Chopin <seb@nuxt.com>
2024-01-19 16:10:48 +0100

    feat: Handle GitHub flavoured markdown blockquotes

--- app/server/plugins/content.ts
+++ app/server/plugins/content.ts
@@ -8,6 +8,16 @@ export default defineNitroPlugin((nitroApp) => {
       if (file.body?.children?.[0]?.tag === 'p') {
         file.body.children.shift()
       }
+      // Handle GitHub flavoured markdown blockquotes
+      // https://github.com/orgs/community/discussions/16925
+      for (const node of (file.body?.children || [])) {
+        if (node.tag === 'blockquote' && // bloquote > p x 2 > span > text
+          ['!NOTE', '!TIP', '!IMPORTANT', '!WARNING', '!CAUTION'].includes(node.children?.[0]?.children?.[0]?.children?.[0]?.value)) {
+            node.type = 'element'
+            node.tag = node.children?.[0]?.children?.[0]?.children?.[0]?.value.slice(1).toLowerCase()
+            node.children[0].children.shift()
+          }
+      }
     }
   })
 })
