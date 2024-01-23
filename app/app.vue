40f2ac04fd7a62f24580d07b502157edf9a01010
barbapapazes <esteban.soubiran@insa-cvl.fr>
2024-01-23 10:24:58 +0100

    fix: correct path

--- app/app.vue
+++ app/app.vue
@@ -25,7 +25,7 @@ useHead({
     { name: 'viewport', content: 'width=device-width, initial-scale=1' },
     { name: 'theme-color', content: _theme[500] },
   ],
-  link: [{ rel: 'icon', href: appConfig.docs.icon }],
+  link: [{ rel: 'icon', href: appConfig.docs.logo }],
 })
 
 useSeoMeta({
