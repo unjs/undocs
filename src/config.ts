2ab5604e52de0352d5b2cd596d09a734e7afa13a
Estéban <esteban.soubiran@insa-cvl.fr>
2024-01-24 14:24:10 +0100

    chore: add comments to types (#13)

--- src/config.ts
+++ src/config.ts
@@ -1,9 +1,29 @@
 import { loadConfig, createDefineConfig } from 'c12'
 
 export interface DocsConfig {
+  /**
+   * The name of the documentation site.
+   *
+   * @example 'UnJS Docs'
+   */
   name?: string
+  /**
+   * The description of the documentation site.
+   *
+   * @example 'Default documentation for UnJS package.'
+   */
   description?: string
+  /**
+   * The GitHub repository for the documentation site.
+   *
+   * @example 'unjs/docs'
+   */
   github?: string
+  /**
+   * Redirects for the documentation site.
+   *
+   * @example { '/foo': '/bar' }
+   */
   redirects?: Record<string, string>
 }
 
