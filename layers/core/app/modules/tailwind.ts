3a35c7969c667088f012d3fa5e8074ee66e282f4
Pooya Parsa <pooya@pi0.io>
2024-02-01 18:11:50 +0100

    fix: workaround for nuxt/ui#809

--- layers/core/app/modules/tailwind.ts
+++ layers/core/app/modules/tailwind.ts
@@ -1,5 +1,16 @@
 import { defineNuxtModule } from 'nuxt/kit'
 
+// Remove deprecated color warnings for Bun
+// https://github.com/nuxt/ui/issues/809
+import { createRequire } from 'node:module'
+const _require = createRequire(import.meta.url)
+const defaultColors = _require('tailwindcss/colors.js')
+delete defaultColors.lightBlue
+delete defaultColors.warmGray
+delete defaultColors.trueGray
+delete defaultColors.coolGray
+delete defaultColors.blueGray
+
 export default defineNuxtModule({
   setup(_, nuxt) {
     if (nuxt.options._prepare) {
