81c720827c7e5d4be3a558804ea41eb49f8d1317
Pooya Parsa <pooya@pi0.io>
2024-02-01 01:16:34 +0100

    fix: restore color

--- app/components/color-picker/ColorPicker.vue
+++ app/components/color-picker/ColorPicker.vue
@@ -2,7 +2,13 @@
 <template>
   <UPopover mode="hover">
     <template #default="{ open }">
-      <UButton color="gray" variant="ghost" square :class="[open && 'bg-gray-50 dark:bg-gray-800']" aria-label="Color picker">
+      <UButton
+        color="gray"
+        variant="ghost"
+        square
+        :class="[open && 'bg-gray-50 dark:bg-gray-800']"
+        aria-label="Color picker"
+      >
         <UIcon name="i-heroicons-paint-brush-20-solid" class="w-5 h-5 text-primary-500 dark:text-primary-400" />
       </UButton>
     </template>
@@ -10,16 +16,28 @@
     <template #panel>
       <div class="p-2">
         <div class="grid grid-cols-5 gap-px">
-          <ColorPickerPill v-for="color in primaryColors" :key="color.value" :color="color" :selected="primary" @select="primary = color" />
+          <ColorPickerPill
+            v-for="color in primaryColors"
+            :key="color.value"
+            :color="color"
+            :selected="primary"
+            @select="primary = color"
+          />
         </div>
 
-        <hr class="border-gray-200 dark:border-gray-800 my-2">
+        <hr class="border-gray-200 dark:border-gray-800 my-2" />
 
         <div class="grid grid-cols-5 gap-px">
-          <ColorPickerPill v-for="color in grayColors" :key="color.value" :color="color" :selected="gray" @select="gray = color" />
+          <ColorPickerPill
+            v-for="color in grayColors"
+            :key="color.value"
+            :color="color"
+            :selected="gray"
+            @select="gray = color"
+          />
         </div>
 
-        <hr class="border-gray-200 dark:border-gray-800 my-2">
+        <hr class="border-gray-200 dark:border-gray-800 my-2" />
 
         <UColorModeToggle />
       </div>
@@ -35,27 +53,48 @@ const colorMode = useColorMode()
 
 // Computed
 
-const primaryColors = computed(() => appConfig.ui.colors.filter(color => color !== 'primary').map(color => ({ value: color, text: color, hex: colors[color][colorMode.value === 'dark' ? 400 : 500] })))
+const primaryColors = computed(() =>
+  appConfig.ui.colors
+    .filter((color) => color !== 'primary')
+    .map((color) => ({ value: color, text: color, hex: colors[color][colorMode.value === 'dark' ? 400 : 500] })),
+)
 const primary = computed({
-  get () {
-    return primaryColors.value.find(option => option.value === appConfig.ui.primary)
+  get() {
+    return primaryColors.value.find((option) => option.value === appConfig.ui.primary)
   },
-  set (option) {
+  set(option) {
     appConfig.ui.primary = option.value
 
     window.localStorage.setItem('nuxt-ui-primary', appConfig.ui.primary)
-  }
+  },
 })
 
-const grayColors = computed(() => ['slate', 'cool', 'zinc', 'neutral', 'stone'].map(color => ({ value: color, text: color, hex: colors[color][colorMode.value === 'dark' ? 400 : 500] })))
+const grayColors = computed(() =>
+  ['slate', 'cool', 'zinc', 'neutral', 'stone'].map((color) => ({
+    value: color,
+    text: color,
+    hex: colors[color][colorMode.value === 'dark' ? 400 : 500],
+  })),
+)
 const gray = computed({
-  get () {
-    return grayColors.value.find(option => option.value === appConfig.ui.gray)
+  get() {
+    return grayColors.value.find((option) => option.value === appConfig.ui.gray)
   },
-  set (option) {
+  set(option) {
     appConfig.ui.gray = option.value
 
     window.localStorage.setItem('nuxt-ui-gray', appConfig.ui.gray)
+  },
+})
+
+onMounted(() => {
+  const primary = window.localStorage.getItem('nuxt-ui-primary')
+  if (primary) {
+    appConfig.ui.primary = primary
+  }
+  const gray = window.localStorage.getItem('nuxt-ui-gray')
+  if (gray) {
+    appConfig.ui.gray = gray
   }
 })
 </script>
--- app/components/color-picker/ColorPickerPill.vue
+++ app/components/color-picker/ColorPickerPill.vue
@@ -8,9 +8,9 @@
         color: {
           white: {
             solid: 'ring-0 bg-gray-100 dark:bg-gray-800 hover:bg-gray-100 dark:hover:bg-gray-800',
-            ghost: 'hover:bg-gray-50 dark:hover:bg-gray-800/50'
-          }
-        }
+            ghost: 'hover:bg-gray-50 dark:hover:bg-gray-800/50',
+          },
+        },
       }"
       :variant="color.value === selected.value ? 'solid' : 'ghost'"
       @click.stop.prevent="$emit('select')"
@@ -21,6 +21,6 @@
 </template>
 
 <script setup lang="ts">
-defineProps<{ color: { value: string, hex: string }, selected: { value: string} }>()
+defineProps<{ color: { value: string; hex: string }; selected: { value: string } }>()
 defineEmits(['select'])
 </script>
