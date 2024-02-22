43f5bdd6185cf947c09d7132a0508108b22aaaae
Pooya Parsa <pooya@pi0.io>
2024-02-22 21:28:40 +0100

    remove log

--- app/tailwind.config.ts
+++ app/tailwind.config.ts
@@ -5,8 +5,6 @@ import defaultTheme from 'tailwindcss/defaultTheme'
 // eslint-disable-next-line unicorn/prefer-module
 const uiDir = resolve(require.resolve('@nuxt/ui'), '..')
 
-console.log(uiDir)
-
 // eslint-disable-next-line unicorn/prefer-module
 const uiProDir = resolve(require.resolve('@nuxt/ui-pro'), '..')
 
