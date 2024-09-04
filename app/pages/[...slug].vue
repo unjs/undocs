ea74abb80376503d07d4ecf448fd7d99b48917c5
Pooya Parsa <pooya@pi0.io>
2024-09-04 13:23:51 +0200

    fix toc highlight

--- app/pages/[...slug].vue
+++ app/pages/[...slug].vue
@@ -33,17 +33,29 @@ usePageSEO({
 const headline = computed(() => findPageHeadline(page.value))
 
 const tocOpen = ref(false)
-const tocLinks = computed(() =>
-  (page.value.body?.toc?.links || []).map((link) => ({
-    label: link.text,
-    href: `#${link.id}`,
-  })),
-)
 
-const scrollToTop = () => {
-  window.scrollTo({ top: 0, left: 0, behavior: 'smooth' })
-}
+const tocLinks = computed(() => {
+  return [
+    [
+      {
+        label: 'Return to top', click: () => {
+          window.scrollTo({ top: 0, left: 0, behavior: 'smooth' })
+        }
+      }
+    ],
+    (page.value.body?.toc?.links || []).map((link) => ({
+      label: link.text,
+      // href: `#${link.id}`,
+      click: () => {
+        tocOpen.value = false
+        document.getElementById(link.id)?.scrollIntoView({ behavior: 'smooth' })
+      },
+    }))
+  ]
+})
+
 const isMobile = ref(false)
+
 onMounted(() => {
   isMobile.value = 'ontouchstart' in document.documentElement
 })
@@ -53,22 +65,12 @@ onMounted(() => {
   <UPage v-if="page">
     <UPageHeader :title="page.title" :description="page.description" :links="page.links" :headline="headline" />
 
-    <div
-      v-if="tocLinks.length > 1"
-      class="float-right mt-4 top-[calc(var(--header-height)_+_0.5rem)] z-10 flex justify-end sticky"
-    >
-      <UDropdown
-        v-model:open="tocOpen"
-        :items="[[{ label: 'Return to top', click: scrollToTop }], tocLinks]"
-        :popper="{ placement: 'bottom-end' }"
-        :mode="isMobile ? 'click' : 'hover'"
-      >
-        <UButton
-          color="white"
-          label="On this page"
-          :trailing="false"
-          :icon="`i-heroicons-chevron-${tocOpen ? 'down' : 'left'}-20-solid`"
-        />
+    <div v-if="tocLinks.length > 1"
+      class="float-right mt-4 top-[calc(var(--header-height)_+_0.5rem)] z-10 flex justify-end sticky">
+      <UDropdown v-model:open="tocOpen" :items="tocLinks" :popper="{ placement: 'bottom-end' }"
+        :mode="isMobile ? 'click' : 'hover'">
+        <UButton color="white" label="On this page" :trailing="false"
+          :icon="`i-heroicons-chevron-${tocOpen ? 'down' : 'left'}-20-solid`" />
       </UDropdown>
     </div>
 
@@ -80,17 +82,14 @@ onMounted(() => {
       <div class="space-y-6">
         <UDivider type="dashed" />
         <div class="mb-4">
-          <UPageLinks
-            class="inline-block"
-            :links="[
-              {
-                icon: 'i-ph-pen-duotone',
-                label: 'Edit this page on GitHub',
-                to: `https://github.com/${appConfig.docs.github}/edit/main/docs/${page._file}`,
-                target: '_blank',
-              },
-            ]"
-          />
+          <UPageLinks class="inline-block" :links="[
+    {
+      icon: 'i-ph-pen-duotone',
+      label: 'Edit this page on GitHub',
+      to: `https://github.com/${appConfig.docs.github}/edit/main/docs/${page._file}`,
+      target: '_blank',
+    },
+  ]" />
         </div>
         <UContentSurround v-if="surround?.length" class="mb-4" :surround="surround" />
       </div>
