efa2fcf1b6f1c97b5d2107493ba8401ede158266
Pooya Parsa <pooya@pi0.io>
2026-01-14 19:48:13 +0100

    update module

--- app/modules/md-rewrite.ts
+++ app/modules/md-rewrite.ts
@@ -1,22 +1,32 @@
+import type { NitroModule } from 'nitropack'
+import { resolve } from 'node:path'
+import { readFile, writeFile } from 'node:fs/promises'
+
 import { defineNuxtModule } from 'nuxt/kit'
 
 export default defineNuxtModule((_options, nuxt) => {
-  nuxt.hooks.hook('nitro:init', (nitro) => {
-    if (nitro.options.dev || !nitro.options.preset.includes('vercel')) {
-      return
-    }
-    nitro.hooks.hook('compiled', async () => {
-      const { resolve } = process.getBuiltinModule('node:path')
-      const { readFile, writeFile } = process.getBuiltinModule('node:fs/promises')
-      const vcJSON = resolve(nitro.options.output.dir, 'config.json')
-      const vcConfig = JSON.parse(await readFile(vcJSON, 'utf8'))
-      vcConfig.routes.unshift({
-        src: '^/(.*)$',
-        dest: '/raw/$1.md',
-        has: [{ type: 'header', key: 'accept', value: '(.*)text/markdown(.*)' }],
-        check: true,
-      })
-      await writeFile(vcJSON, JSON.stringify(vcConfig, null, 2), 'utf8')
-    })
-  })
+  nuxt.options.nitro.modules ??= []
+  nuxt.options.nitro.modules.push(mdRewrite())
 })
+
+function mdRewrite(): NitroModule {
+  return {
+    name: 'markdown-rewrite',
+    setup(nitro) {
+      if (nitro.options.dev || !nitro.options.preset.includes('vercel')) {
+        return
+      }
+      nitro.hooks.hook('compiled', async () => {
+        const vcJSON = resolve(nitro.options.output.dir, 'config.json')
+        const vcConfig = JSON.parse(await readFile(vcJSON, 'utf8'))
+        vcConfig.routes.unshift({
+          src: '^/(.*)$',
+          dest: '/raw/$1.md',
+          has: [{ type: 'header', key: 'accept', value: '(.*)text/markdown(.*)' }],
+          check: true,
+        })
+        await writeFile(vcJSON, JSON.stringify(vcConfig, null, 2), 'utf8')
+      })
+    },
+  }
+}
